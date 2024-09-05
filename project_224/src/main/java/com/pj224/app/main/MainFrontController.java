a62a8aa45ffcbd26162a99ade8b49f99ff43c92d
ssm0971 <ssm0971@naver.com>
2024-09-05 09:47:12 +0900

    240905신승민 메인컨트롤러 메인브랜치에서 잘못건드림

--- project_224/src/main/java/com/pj224/app/main/MainFrontController.java
+++ project_224/src/main/java/com/pj224/app/main/MainFrontController.java
@@ -1,8 +1,8 @@
 package com.pj224.app.main;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MainFrontController
  */
-@WebServlet("/MainFrontController")
+//@WebServlet("/MainFrontController")
 public class MainFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +27,9 @@ public class MainFrontController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request, response);
+		
 	}
 
 	/**
@@ -35,7 +37,60 @@ public class MainFrontController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		doGet(request, response);
+		doProcess(request, response);
+	}
+	
+	protected void doProcess (HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+		System.out.println("Main 서블릿 실행");
+
+		// request.getContextPath() URL루트경로를 의미
+		System.out.println(request.getContextPath());
+
+		// request.getRequestURI 현재 요청의 URI를 의미
+		System.out.println(request.getRequestURI());
+
+		// 전체 URI에서 ContextPath를 제외시킨 부분만 변수에 저장(분기처리할 때 비교할 변수로 사용)
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println(target);
+
+//		Result result = null; // Result 클래스 객체
+		
+		switch (target) {
+		case "/member/" :
+			
+			break;
+		case "/member//" :
+			
+			break;
+		case "/member//m/" :
+			
+			break;
+		case "/member///" :
+			
+			break;
+		case "/member////" :
+			
+			break;
+		case "/member//m//" :
+			
+			break;
+		case "/member/////" :
+			
+			break;
+			
+		
+
+		default:
+			break;
+			
+			
+		}
+		
+		
+		
+		
+		
 	}
 
 }
