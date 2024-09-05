42cef2a5c8c2cc1ca2b57cd3a3cdd7aa64101f98
Gilbert <gilbert.dev.kr@gmail.com>
2024-09-05 09:25:40 +0900

    20240905 이기철 커뮤니티작업

--- project_224/src/main/java/com/pj224/app/dao/CommunityDAO.java
+++ project_224/src/main/java/com/pj224/app/dao/CommunityDAO.java
@@ -1,5 +1,26 @@
 package com.pj224.app.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.mybatis.config.MyBatisConfig;
+import com.pj224.app.dto.CommunityDTO;
+
 public class CommunityDAO {
+
+	private SqlSession sqlSession;
+
+	public CommunityDAO() {
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+
+	public List<CommunityDTO> selectAll() {
+		
+	    return sqlSession.selectList("community.selectAll");
+	    
+	}
+	
+	
 
 }
--- project_224/src/main/java/com/pj224/app/dto/CommunityDTO.java
+++ project_224/src/main/java/com/pj224/app/dto/CommunityDTO.java
@@ -2,4 +2,83 @@ package com.pj224.app.dto;
 
 public class CommunityDTO {
 
+//	   BOARD_NUMBER NUMBER PRIMARY KEY,
+//	   BOARD_CATE VARCHAR2(1000) NOT NULL,
+//	   BOARD_TITLE VARCHAR2(1000) NOT NULL,
+//	   BOARD_CONTENT VARCHAR2(1000) NOT NULL,
+//	   BOARD_REGIST_DATE DATE DEFAULT SYSDATE,
+//	   BOARD_UPDATE_DATE DATE DEFAULT SYSDATE,
+//	   MEMBER_NUMBER NUMBER NOT NULL,  
+
+	private int boardNumber;
+	private String boardCate;
+	private String boardTitle;
+	private String boardContent;
+	private String boardRegistDate;
+	private String boardUpdateDate;
+	private int memberNumber;
+
+	public int getBoardNumber() {
+		return boardNumber;
+	}
+
+	public void setBoardNumber(int boardNumber) {
+		this.boardNumber = boardNumber;
+	}
+
+	public String getBoardCate() {
+		return boardCate;
+	}
+
+	public void setBoardCate(String boardCate) {
+		this.boardCate = boardCate;
+	}
+
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+
+	public String getBoardContent() {
+		return boardContent;
+	}
+
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+
+	public String getBoardRegistDate() {
+		return boardRegistDate;
+	}
+
+	public void setBoardRegistDate(String boardRegistDate) {
+		this.boardRegistDate = boardRegistDate;
+	}
+
+	public String getBoardUpdateDate() {
+		return boardUpdateDate;
+	}
+
+	public void setBoardUpdateDate(String boardUpdateDate) {
+		this.boardUpdateDate = boardUpdateDate;
+	}
+
+	public int getMemberNumber() {
+		return memberNumber;
+	}
+
+	public void setMemberNumber(int memberNumber) {
+		this.memberNumber = memberNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardDTO [boardNumber=" + boardNumber + ", boardCate=" + boardCate + ", boardTitle=" + boardTitle
+				+ ", boardContent=" + boardContent + ", boardRegistDate=" + boardRegistDate + ", boardUpdateDate="
+				+ boardUpdateDate + ", memberNumber=" + memberNumber + "]";
+	}
+
 }
