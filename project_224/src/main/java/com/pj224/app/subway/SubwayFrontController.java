9f8d09bee0b0975f5efae03345fe5af20580c242
ssm0971 <ssm0971@naver.com>
2024-09-06 10:38:02 +0900

    240906신승민 헤더 네비게이션 작업

--- project_224/src/main/java/com/pj224/app/subway/SubwayFrontController.java
+++ project_224/src/main/java/com/pj224/app/subway/SubwayFrontController.java
@@ -41,7 +41,31 @@ public class SubwayFrontController extends HttpServlet {
 	}
 	
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Subway 서블릿 실행");
+
+		// request.getContextPath() URL루트경로를 의미
+		System.out.println(request.getContextPath());
+
+		// request.getRequestURI 현재 요청의 URI를 의미
+		System.out.println(request.getRequestURI());
+
+		// 전체 URI에서 ContextPath를 제외시킨 부분만 변수에 저장(분기처리할 때 비교할 변수로 사용)
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println(target);
 		
+		switch (target) {
+		case "/subway/subway-route.sw":
+			System.out.println("2호선노선도");
+			request.getRequestDispatcher("/app/subway/subway-route.jsp").forward(request, response);
+			break;
+		case "/subway/subway-schedule.sw":
+			System.out.println("2호선배차시간");
+			request.getRequestDispatcher("/app/subway/subway-schedule.jsp").forward(request, response);
+			break;
+
+		default:
+			break;
+		}
 		
 		
 	}
