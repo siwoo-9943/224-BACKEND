d2459494a3f8017a426bbdfcf0ad60eacc7210ad
krystal0093 <krystalline0093@gmail.com>
2024-09-08 14:45:45 +0900

    20240908 강수정 공지쏙쏙글작성중

--- project_224/src/main/java/com/pj224/app/notice/NoticeFrontController.java
+++ project_224/src/main/java/com/pj224/app/notice/NoticeFrontController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.pj224.app.Result;
 import com.pj224.app.community.CommunityListOkController;
+import com.pj224.app.community.CommunityWriteOkController;
 import com.pj224.app.main.MainLoginController;
 import com.pj224.app.main.MainLogoutController;
 import com.pj224.app.member.LoginOkController;
@@ -64,9 +65,17 @@ public class NoticeFrontController extends HttpServlet {
 			result = new NoticeDetailController().MemExecute(request, response);
 			request.getRequestDispatcher("/app/notice/notice-addetail.jsp").forward(request, response);
 			break;
+//		case "/notice/write.no":
+//			System.out.println("write");
+//			response.sendRedirect(request.getContextPath() + "/app/notice/notice-adwrite.jsp");
+//			break;
 		case "/notice/write.no":
 			System.out.println("write");
-			response.sendRedirect(request.getContextPath() + "/app/notice/notice-adwrite.jsp");
+			request.getRequestDispatcher("/app/notice/notice-adwrite.jsp").forward(request, response);
+			break;
+		case "/notice/writeOk.no":
+			System.out.println("writeOk");
+			result = new NoticeWriteOkController().MemExecute(request, response);
 			break;
 //		case "/notice/writeOk.no":
 //			System.out.println("writeOk");
--- project_224/src/main/java/com/pj224/app/notice/NoticeWriteOkController.java
+++ project_224/src/main/java/com/pj224/app/notice/NoticeWriteOkController.java
@@ -1,5 +1,47 @@
 package com.pj224.app.notice;
 
-public class NoticeWriteOkController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pj224.app.MemExecute;
+import com.pj224.app.Result;
+import com.pj224.app.dao.CommunityDAO;
+import com.pj224.app.dto.CommunityDTO;
+
+public class NoticeWriteOkController implements MemExecute {
+	
+	@Override
+	public Result MemExecute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		System.out.println("글작성 컨트롤러 들어왔냐??");
+		request.setCharacterEncoding("UTF-8");
+		
+		System.out.println("cate값 " + request.getParameter("boardCate"));
+		
+		CommunityDAO communityDAO = new CommunityDAO();
+		CommunityDTO communityDTO = new CommunityDTO();
+		Result result = new Result();
+
+		communityDTO.setBoardCate(request.getParameter("boardCate"));
+		communityDTO.setBoardTitle(request.getParameter("boardTitle"));
+		communityDTO.setBoardContent(request.getParameter("editordata"));
+		communityDTO.setBoardRegistDate(request.getParameter("boardRegistDate"));
+		communityDTO.setBoardUpdateDate(request.getParameter("boardUpdateDate"));
+		communityDTO.setMemberNumber(Integer.valueOf(request.getParameter("memberNumber")));
+
+		request.setAttribute("boardCate", communityDTO.getBoardCate());
+		request.setAttribute("boardTile", communityDTO.getBoardTitle());
+		request.setAttribute("boardContent", communityDTO.getBoardContent());
+
+		// 데이터베이스에 저장
+		communityDAO.insert(communityDTO);
+
+		// 결과 설정
+		result.setRedirect(true);
+		result.setPath(request.getContextPath() + "/notice/notice-admain.no");
+
+		return result;
+	}
 }
