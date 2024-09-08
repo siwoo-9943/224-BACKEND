02e5a67e55f32410b35d5ddc54c26b3e159d560f
ssm0971 <ssm0971@naver.com>
2024-09-08 17:18:21 +0900

    240908신승민 메인페이지 커뮤니티 컨텐츠 조회

---
+++ project_224/src/main/java/com/pj224/app/main/MainComuListController.java
@@ -0,0 +1,36 @@
+package com.pj224.app.main;
+
+import java.io.IOException;
+import java.rmi.ServerException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pj224.app.MemExecute;
+import com.pj224.app.Result;
+import com.pj224.app.dao.MainDAO;
+import com.pj224.app.dto.MainDTO;
+
+public class MainComuListController implements MemExecute{
+
+	@Override
+	public Result MemExecute(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServerException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+	    MainDAO mainDAO = new MainDAO();
+	    
+	    Result result = new Result();
+	    
+	    List<MainDTO> comuListResults;
+	    comuListResults = mainDAO.maincomulist();
+	    System.out.println("목록 \n" + comuListResults);
+		
+	    request.setAttribute("comuListResults", comuListResults);
+
+		return result;
+	}
+	
+
+}
