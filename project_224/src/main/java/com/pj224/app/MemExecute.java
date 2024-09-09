bd0755deace907167b3566a3e00ab26326be595b
kimminchae <0720kmc@naver.com>
2024-09-09 11:18:04 +0900

    20240908 김민채 아이디찾기완료

--- project_224/src/main/java/com/pj224/app/MemExecute.java
+++ project_224/src/main/java/com/pj224/app/MemExecute.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface MemExecute {
-    public Result MemExecute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServerException;
+    public Result MemExecute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServerException, ServletException;
 
 }
 
