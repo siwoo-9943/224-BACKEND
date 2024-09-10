883ce875529ecdc554b4488f03a6ad7ade4647de
Gilbert <gilbert.dev.kr@gmail.com>
2024-09-10 16:10:38 +0900

    커밋

--- project_224/src/main/webapp/assets/js/modal/notice-modal.js
+++ project_224/src/main/webapp/assets/js/modal/notice-modal.js
@@ -19,31 +19,3 @@ window.onclick = function(event) {
 	}
 };
 
-$(document).ready(function() {
-  const $modal = $('#notice-deleteModal');
-  let currentNoticeNumber;
-
-  // 삭제 모달 열기
-  window.openDeleteModal = function(noticeNumber) {
-    currentNoticeNumber = noticeNumber;
-    $modal.css('display', 'block');
-  };
-
-  // 삭제 모달 닫기
-  window.closeDeleteModal = function() {
-    $modal.css('display', 'none');
-  };
-
-  // 삭제 확인
-  window.confirmDelete = function() {
-    $('#noticeNumberInput').val(currentNoticeNumber);
-    $('#deleteForm').submit();
-  };
-
-  // 모달 외부 클릭 시 닫기
-  $(window).on('click', function(event) {
-    if ($(event.target).is($modal)) {
-      closeDeleteModal();
-    }
-  });
-});
